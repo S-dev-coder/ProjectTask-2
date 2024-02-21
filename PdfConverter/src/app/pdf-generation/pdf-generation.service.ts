062a2f37b119ec2f05eb972e25eb2c324819ca21
s-dev-coder <sk7137389@gmail.com>
2024-02-21 14:51:42 +0530

    Integrated PDF download feature: -Introduced a new method downloadPdf to handle PDF download functionality. -Created a new Observable to manage the HTTP POST request. -Integrated PDF download functionality within the Observable's success callback. -Cleaned up the code by removing redundant code blocks and improving readability. -Added error handling for file reading failures.

--- PdfConverter/src/app/pdf-generation/pdf-generation.service.ts
+++ PdfConverter/src/app/pdf-generation/pdf-generation.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class PdfGenerationService {
   private API_KEY = "sk7137389@gmail.com_Vb7482S3P75KS97fJ735klw9hoAUpBu1T19D43Fq74R59J8IA4r100gYYijY1AZv";
+  private SourceUrl: string = "http://en.wikipedia.org/wiki/Main_Page";
 
   constructor(private http: HttpClient) { }
 
@@ -25,8 +26,44 @@ export class PdfGenerationService {
       }
     };
 
-    return this.http.post<any>(API_URL, jsonPayload, options);
+    return new Observable<any>((observer) => {
+      this.http.post<any>(API_URL, jsonPayload, options).subscribe(
+        (response) => {
+          if (!response.error) {
+            this.downloadPdf(response.url);
+            console.log("successfully saved the file")
+          }
+          observer.next(response);
+          observer.complete();
+        },
+        (error) => {
+          observer.error(error);
+        }
+      );
+    });
   }
-}
 
+  downloadPdf(url: string): void {
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      if (fileReader.result) {
+        const blob = new Blob([fileReader.result], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'result.pdf';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      } else {
+        console.error('Failed to read file content.');
+      }
+    };
+    this.http.get(url, { responseType: 'blob' }).subscribe((response) => {
+      fileReader.readAsArrayBuffer(response);
+    });
+  }
+
+}
 
