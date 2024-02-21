155e6e875b8313f8cceb883362b65a09298f498b
s-dev-coder <sk7137389@gmail.com>
2024-02-21 14:58:24 +0530

    Enhanced PDF generation functionality and error handling: -Optimized the PDF generation component for better performance. -Implemented a delay of 7 seconds to ensure smoother processing. -Enhanced error handling to provide more informative messages. -Updated console logs for clearer indication of program flow.

--- PdfConverter/src/app/pdf-generation/pdf-generator.component.ts
+++ PdfConverter/src/app/pdf-generation/pdf-generator.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PdfGenerationService } from './pdf-generation.service';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'pdf-generator',
@@ -15,6 +16,7 @@ export class PdfGeneratorComponent {
   constructor(private pdfService: PdfGenerationService) { }
 
   generatePdf() {
+    console.log("Url" + this.url)
     if (!this.url.trim()) {
       this.error = 'Please enter a valid URL';
       return;
@@ -22,25 +24,22 @@ export class PdfGeneratorComponent {
 
     this.loading = true;
     this.error = null;
+    
 
-    this.pdfService.generatePdfFromUrl(this.url).subscribe(
-      (pdfBlob: Blob) => {
-        this.downloadPdf(pdfBlob);
+    this.pdfService.generatePdfFromUrl(this.url).pipe(
+      delay(7000) // Delay for 2000 milliseconds (2 seconds)
+    ).subscribe(
+      () => {
         this.loading = false;
+        this.url = '';
+        confirm("Pdf Generated Successfully");
       },
-      (      error: any) => {
+      () => {
         this.error = 'An error occurred while generating the PDF';
         this.loading = false;
       }
     );
-  }
-
-  downloadPdf(pdfBlob: Blob) {
-    const downloadLink = document.createElement('a');
-    downloadLink.href = window.URL.createObjectURL(pdfBlob);
-    downloadLink.download = 'generated_pdf.pdf';
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
+    
+    console.log("Successfully Converted!!");
   }
 }
